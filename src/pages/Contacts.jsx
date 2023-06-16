c590dd6c5e82a13cf9b3b92676cfeca2ee4813ac
SantoshChapagai <santoshchapagai100@gmail.com>
2023-06-16 07:39:29 +0300

    tailwind grid card practice

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -4,43 +4,62 @@ const Contacts = () => {
   return (
     <div className='bg-orange-200 h-screen'>
       <h2 className='py-5 text-center'>Contact us</h2>
-      <div className='grid grid-cols-3 gap-5 p-5'>
-        <div>
+      <div className='grid grid-cols-3 gap-5 p-5 mx-5'>
+        <div className='bg-orange-300 p-5 rounded-md flex flex-col justify-center items-center w-18'>
           <h2>Santosh Chapagai</h2>
           <p>Software Developer</p>
           <p>Tel: 000000000</p>
           <p>Email: Santosh@bc.fi</p>
         </div>
-        <div>
+        <div className='bg-orange-300 p-5 rounded-md flex flex-col justify-center items-center w-18'>
           <h2>Santosh Chapagai</h2>
           <p>Software Developer</p>
           <p>Tel: 000000000</p>
           <p>Email: Santosh@bc.fi</p>
         </div>
-        <div>
+        <div className='bg-orange-300 p-5 rounded-md flex flex-col justify-center items-center w-18'>
           <h2>Santosh Chapagai</h2>
           <p>Software Developer</p>
           <p>Tel: 000000000</p>
           <p>Email: Santosh@bc.fi</p>
         </div>
-        <div>
+        <div className='bg-orange-300 p-5 rounded-md flex flex-col justify-center items-center w-18'>
           <h2>Santosh Chapagai</h2>
           <p>Software Developer</p>
           <p>Tel: 000000000</p>
           <p>Email: Santosh@bc.fi</p>
         </div>
-        <div>
+        <div className='bg-orange-300 p-5 rounded-md flex flex-col justify-center items-center w-18'>
           <h2>Santosh Chapagai</h2>
           <p>Software Developer</p>
           <p>Tel: 000000000</p>
           <p>Email: Santosh@bc.fi</p>
         </div>
-        <div>
+        <div className='bg-orange-300 p-5 rounded-md flex flex-col justify-center items-center w-18'>
           <h2>Santosh Chapagai</h2>
           <p>Software Developer</p>
           <p>Tel: 000000000</p>
           <p>Email: Santosh@bc.fi</p>
         </div>
+        <div className='bg-orange-300 p-5 rounded-md flex flex-col justify-center items-center w-18'>
+          <h2>Santosh Chapagai</h2>
+          <p>Software Developer</p>
+          <p>Tel: 000000000</p>
+          <p>Email: Santosh@bc.fi</p>
+        </div>
+        <div className='bg-orange-300 p-5 rounded-md flex flex-col justify-center items-center w-18'>
+          <h2>Santosh Chapagai</h2>
+          <p>Software Developer</p>
+          <p>Tel: 000000000</p>
+          <p>Email: Santosh@bc.fi</p>
+        </div>
+        <div className='bg-orange-300 p-5 rounded-md flex flex-col justify-center items-center w-18'>
+          <h2>Santosh Chapagai</h2>
+          <p>Software Developer</p>
+          <p>Tel: 000000000</p>
+          <p>Email: Santosh@bc.fi</p>
+        </div>
+
       </div>
     </div>
   );
