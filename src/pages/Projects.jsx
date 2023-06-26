33b9803b1257ee70e5430ffa65c77c9c4f1da333
SantoshChapagai <santoshchapagai100@gmail.com>
2023-06-26 17:54:44 +0300

    tailwind practice

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 const Projects = () => {
   return (
-    <div className='bg-lime-100 h-screen'>
+    <div className='bg-lime-100 h-screen p-5'>
       <div className='bg-white w-48 h-96'>
         <img className='h-24 w-48' src="https://cdn.pixabay.com/photo/2015/12/07/10/24/go-kart-1080492_1280.jpg" alt='front-end' />
-        <h2>Speed Game</h2>
-        <p>HTML, CSS, Javascript</p>
+        <h2 className='p-2'>Speed Game</h2>
+        <p className='p-2'>HTML, CSS, Javascript</p>
+        <p className='p-2'>Game that test your speed skills. There are four circles in the game. Circles activate randomly. If the player click the right activate circle the score increases by 10.</p>
+
 
       </div>
     </div>
