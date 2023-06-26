c5e748e0a59f6d17382e5e42df35dff95ee9056b
SantoshChapagai <santoshchapagai100@gmail.com>
2023-06-26 18:03:35 +0300

    tailwind practice

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,7 +2,61 @@ import React from 'react';
 
 const Projects = () => {
   return (
-    <div className='bg-lime-100 h-screen p-5'>
+    <div className='bg-lime-100 p-5 grid grid-cols-4'>
+      <div className='bg-white w-48 h-fit mx-2 my-2'>
+        <img className='h-24 w-48' src="https://cdn.pixabay.com/photo/2015/12/07/10/24/go-kart-1080492_1280.jpg" alt='front-end' />
+        <h2 className='p-2'>Speed Game</h2>
+        <p className='p-2'>HTML, CSS, Javascript</p>
+        <p className='p-2'>Game that test your speed skills. There are four circles in the game. Circles activate randomly. If the player click the right activate circle the score increases by 10.</p>
+      </div>
+      <div className='bg-white w-48 h-96'>
+        <img className='h-24 w-48' src="https://cdn.pixabay.com/photo/2015/12/07/10/24/go-kart-1080492_1280.jpg" alt='front-end' />
+        <h2 className='p-2'>Receipe App</h2>
+        <p className='p-2'>React, CSS</p>
+        <p className='p-2'>Application that allows user to add the receipe and browse receipes.</p>
+
+
+      </div>
+      <div className='bg-white w-48 h-96'>
+        <img className='h-24 w-48' src="https://cdn.pixabay.com/photo/2015/12/07/10/24/go-kart-1080492_1280.jpg" alt='front-end' />
+        <h2 className='p-2'>Speed Game</h2>
+        <p className='p-2'>HTML, CSS, Javascript</p>
+        <p className='p-2'>Game that test your speed skills. There are four circles in the game. Circles activate randomly. If the player click the right activate circle the score increases by 10.</p>
+
+
+      </div>
+      <div className='bg-white w-48 h-96'>
+        <img className='h-24 w-48' src="https://cdn.pixabay.com/photo/2015/12/07/10/24/go-kart-1080492_1280.jpg" alt='front-end' />
+        <h2 className='p-2'>Speed Game</h2>
+        <p className='p-2'>HTML, CSS, Javascript</p>
+        <p className='p-2'>Game that test your speed skills. There are four circles in the game. Circles activate randomly. If the player click the right activate circle the score increases by 10.</p>
+
+
+      </div>
+      <div className='bg-white w-48 h-96'>
+        <img className='h-24 w-48' src="https://cdn.pixabay.com/photo/2015/12/07/10/24/go-kart-1080492_1280.jpg" alt='front-end' />
+        <h2 className='p-2'>Speed Game</h2>
+        <p className='p-2'>HTML, CSS, Javascript</p>
+        <p className='p-2'>Game that test your speed skills. There are four circles in the game. Circles activate randomly. If the player click the right activate circle the score increases by 10.</p>
+
+
+      </div>
+      <div className='bg-white w-48 h-96'>
+        <img className='h-24 w-48' src="https://cdn.pixabay.com/photo/2015/12/07/10/24/go-kart-1080492_1280.jpg" alt='front-end' />
+        <h2 className='p-2'>Speed Game</h2>
+        <p className='p-2'>HTML, CSS, Javascript</p>
+        <p className='p-2'>Game that test your speed skills. There are four circles in the game. Circles activate randomly. If the player click the right activate circle the score increases by 10.</p>
+
+
+      </div>
+      <div className='bg-white w-48 h-96'>
+        <img className='h-24 w-48' src="https://cdn.pixabay.com/photo/2015/12/07/10/24/go-kart-1080492_1280.jpg" alt='front-end' />
+        <h2 className='p-2'>Speed Game</h2>
+        <p className='p-2'>HTML, CSS, Javascript</p>
+        <p className='p-2'>Game that test your speed skills. There are four circles in the game. Circles activate randomly. If the player click the right activate circle the score increases by 10.</p>
+
+
+      </div>
       <div className='bg-white w-48 h-96'>
         <img className='h-24 w-48' src="https://cdn.pixabay.com/photo/2015/12/07/10/24/go-kart-1080492_1280.jpg" alt='front-end' />
         <h2 className='p-2'>Speed Game</h2>
