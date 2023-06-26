957882fce49cbf3385e7d50e7cba5a270459cdc2
SantoshChapagai <santoshchapagai100@gmail.com>
2023-06-26 17:49:01 +0300

    practice tailwind

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Projects = () => {
   return (
-    <div className='bg-lime-200 h-screen flex justify-center'>
-      Projects will be here
+    <div className='bg-lime-100 h-screen'>
+      <div className='bg-white w-48 h-96'>
+        <img className='h-24 w-48' src="https://cdn.pixabay.com/photo/2015/12/07/10/24/go-kart-1080492_1280.jpg" alt='front-end' />
+        <h2>Speed Game</h2>
+        <p>HTML, CSS, Javascript</p>
+
+      </div>
     </div>
   );
 };
