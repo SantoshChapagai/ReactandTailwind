81998600474aef1a6ab55ea318c4d857cfe93b04
SantoshChapagai <santoshchapagai100@gmail.com>
2023-06-28 19:01:54 +0300

    tailwind practice

--- src/pages/More.jsx
+++ src/pages/More.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const More = () => {
   return (
     <div className='h-screen bg-cyan-100'>
-      More will be here
+      <div className='p-2 flex justify-center'>
+        <h3>More information about tailwind</h3>
+      </div>
+      <div className='p-2'>
+        <p className='flex justify-center'>Tailwind is an awesome inline styling framework. </p>
+        <p className='flex justify-center'>Just using the classname and applying the styling will work. </p>
+      </div>
     </div>
   );
 };
